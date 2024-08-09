ffc6a3ea1cbb32936cdf518a4d1c648f687b16da
Aleх Bark <thrtth@yandex.ru>
2024-08-10 00:04:40 +0700

    Fix lint

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = 'static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    # Tell Django to copy static assets into a path called `staticfiles`
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
